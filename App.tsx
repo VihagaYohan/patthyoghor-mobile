96457b96e2ff92be20eaa02b18e5270691205ba4
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2022-10-02 09:35:32 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+import 'react-native-gesture-handler';
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './app/screens/HomeScreen'
+
+const App = () => {
+  return <SafeAreaView>
+    <HomeScreen/>
+  </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {StyleSheet,View,Text} from 'react-native'
+
+const Screen = () =>{
+   
+    return(
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Screen;
